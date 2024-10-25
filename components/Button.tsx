bbab4746aaa98ee0b4acbc3e6a5f02457085c90c
akuro <akuro2001@gmail.com>
2024-10-25 19:34:42 +0200

    Zad3

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,57 +1,32 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-interface ButtonProperties {
-    label: string;
-    action: () => void;
-    isOperator?: boolean;
-    isReset?: boolean;
+interface ButtonProps {
+    title: string;
+    onPress: () => void;
 }
 
-export default function Button({ label, action, isOperator, isReset }: ButtonProperties) {
+function Button({ title, onPress }: ButtonProps) {
     return (
-        <TouchableOpacity
-            style={[
-                isOperator ? buttonStyles.operatorButton : isReset ? buttonStyles.resetButton : buttonStyles.numberButton,
-                label === "0" ? buttonStyles.zeroButton : ""
-            ]}
-            onPress={action}
-        >
-            <Text style={buttonStyles.textStyle}>{label}</Text>
+        <TouchableOpacity style={buttonTheme.button} onPress={onPress}>
+            <Text style={buttonTheme.text}>{title}</Text>
         </TouchableOpacity>
     );
 }
 
-const buttonStyles = StyleSheet.create({
-    operatorButton: {
-        backgroundColor: '#f39c12',
+const buttonTheme = StyleSheet.create({
+    button: {
+        backgroundColor: '#7c7d7f', 
+        width: 100, 
+        height: 50, 
         justifyContent: 'center',
         alignItems: 'center',
-        flex: 1,
-        marginBottom: 2,
+        borderRadius: 5,
     },
-    numberButton: {
-        backgroundColor: '#7c7d7f',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-        marginRight: 2,
-        marginBottom: 2,
-    },
-    resetButton: {
-        backgroundColor: '#7c7d7f',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-        marginRight: 2,
-        marginBottom: 2,
-    },
-    textStyle: {
+    text: {
         color: 'white',
-        fontSize: 48,
-        padding: 10,
-    },
-    zeroButton: {
-        flex: 2,
-    },
+        fontSize: 16,
+    }
 });
+
+export default Button;
